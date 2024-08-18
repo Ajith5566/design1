724d97df71fa090b9bd4536460296dab226ca174
Ajith5566 <ajithbabu5566@gmail.com>
2024-08-18 23:07:44 +0530

    design1

---
+++ index.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    var parent = this.parentNode; // Get the parent element (#sevth_first_div1)
+    parent.classList.toggle("active"); // Toggle the 'active' class on the parent
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
